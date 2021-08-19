4511a08051ad583cba13dac3e7fa08eaa558183e
Aariv <arivbits@gmail.com>
2021-08-19 19:58:18 +0530

    Worked on Java Concepts

--- Java-Programiz/src/main/java/com/ariv/programiz/exceptionhandling/JavaExceptionHandling.java
+++ Java-Programiz/src/main/java/com/ariv/programiz/exceptionhandling/JavaExceptionHandling.java
@@ -1,5 +1,39 @@
 package com.ariv.programiz.exceptionhandling;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class JavaExceptionHandling {
 
+	public static void main(String[] args) {
+		//1. Try - Catch
+		try {
+			int divideByZero = 5 / 0;
+			System.out.println("Hello");
+		} catch (ArithmeticException e) {
+			System.out.println("Arithmetic Exception" + e.getMessage());
+			// 2. Try - catch - finally
+		} finally {
+			System.out.println("Execute me always");
+		}
+		
+		//divideByZero();
+		
+		try {
+			findFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	// 3. Java Throw - Throws
+	public static void divideByZero() {
+		throw new ArithmeticException();
+	}
+	
+	// throws
+	public static void findFile() throws IOException {
+		File newFile = new File("Test.txt");
+		FileInputStream fis = new FileInputStream(newFile);
+	}
 }
--- Java-Programiz/src/main/java/com/ariv/programiz/exceptionhandling/JavaTryCatch.java
+++ Java-Programiz/src/main/java/com/ariv/programiz/exceptionhandling/JavaTryCatch.java
@@ -1,5 +1,39 @@
 package com.ariv.programiz.exceptionhandling;
 
 public class JavaTryCatch {
-
+	public int[] arr = new int[10];
+	public static void main(String[] args) {
+		try {
+			//System.exit(0); // finally won't be called.
+			int dvZero = 5 / 0;
+		}catch(ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			System.out.println("Finaly block is always executed");
+		} 
+		
+		// multiple catch block
+		
+		JavaTryCatch jcatch = new JavaTryCatch();
+		jcatch.writeList();
+		
+	}
+	
+	public void writeList() {
+	    try {
+	      arr[10] = 11;
+	    }
+	    /**
+	    catch (NumberFormatException e1) {
+	      System.out.println("NumberFormatException => " + e1.getMessage());
+	    }
+	    catch (IndexOutOfBoundsException e2) {
+	      System.out.println("IndexOutOfBoundsException => " + e2.getMessage());
+	    }*/
+	    
+	    catch (NumberFormatException | IndexOutOfBoundsException e2) {
+		      System.out.println("NumberFormatException => " + e2.getMessage());
+		    }
+	}
 }
